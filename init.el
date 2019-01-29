843d21c044ba2f82b67ad737a6b4ecd3126c72fe
Chirag Kantharia <chirag@zededa.com>
2019-01-29 23:03:32 +0530

    Remove custom variables

--- init.el
+++ init.el
@@ -43,18 +43,3 @@
 
 
 ;; /end
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (yaml-mode xkcd xcscope writegood-mode win-switch whitespace-cleanup-mode which-key web-mode volatile-highlights use-package tickscript-mode smex smartparens smart-mode-line session rainbow-mode rainbow-delimiters python-mode py-yapf protobuf-mode projectile org-bullets offlineimap nyan-mode notmuch magithub magit-gh-pulls json-mode js3-mode hl-todo goto-last-change go-guru go-eldoc go-add-tags function-args focus flyspell-correct-ivy flymake-python-pyflakes flycheck expand-region exec-path-from-shell eink-theme dockerfile-mode diminish diff-hl darktooth-theme counsel company-go better-defaults beacon avy auto-compile anaconda-mode alect-themes ag)))
- '(session-use-package t nil (session)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
